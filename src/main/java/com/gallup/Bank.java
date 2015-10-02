3e0c05bd6b9ed4c1481953ed25cc2b5275e42a95
camerica <cameronhickey369@gmail.com>
2015-10-01 22:26:32 -0500

    made some checksytle fixes

--- src/main/java/com/gallup/Bank.java
+++ src/main/java/com/gallup/Bank.java
@@ -3,34 +3,35 @@ package com.gallup;
 import java.util.Scanner;
 
 public class Bank {
-	public static void main(String[] args) {
-		int x = 0;
+	private static Scanner scanner;
+
+	public static void main(final String[] args) {
 		int password = 1234;
 		int passwordGuess = 0;
 		int currSelection = 0;
 		double requestedMoney = 0.00;
 		double balance = 10.00;
 		boolean exitProgram = false;
-		Scanner s = new Scanner(System.in);
+		scanner = new Scanner(System.in);
 		while (password != passwordGuess) {
 			System.out.println("What is your password?");
-			passwordGuess = s.nextInt();
+			passwordGuess = scanner.nextInt();
 			if (passwordGuess == password) {
 				System.out.println("correct password");
 			} else {
 				System.out.println("Incorrect password, please try again");
 			}
-			while (exitProgram == false) {
+			while (exitProgram) {
 				System.out.println("Thank you for banking with Cameron Hickey Banking.");
 				System.out.println("Press 1 to withdraw");
 				System.out.println("Press 2 to add a Deposit");
 				System.out.println("Press 3 to check your balance");
 				System.out.println("Press 4 to open a CD");
 				System.out.println("Press 5 to exit your account and sign in as a different user");
-				currSelection = s.nextInt();
+				currSelection = scanner.nextInt();
 				if (currSelection == 1) {
 					System.out.println("How much money would you like to withdraw?");
-					requestedMoney = s.nextDouble();
+					requestedMoney = scanner.nextDouble();
 
 					if (requestedMoney > balance) {
 						System.out.println("I am sorry, but you do not have the funds to make that withdrawl");
@@ -38,7 +39,7 @@ public class Bank {
 
 					if (requestedMoney < balance) {
 						System.out.println("You have withdrawn $" + Double.toString(requestedMoney)
-								+ "0 dollars from your account");
+						        + "0 dollars from your account");
 						balance = balance - requestedMoney;
 						System.out.println("Your current balance is $" + Double.toString(balance) + "0");
 					}
@@ -46,13 +47,14 @@ public class Bank {
 
 				if (currSelection == 2) {
 					System.out.println("How much money would you like to Deposit?");
-					requestedMoney = s.nextDouble();
+					requestedMoney = scanner.nextDouble();
 					balance = balance + requestedMoney;
 					System.out.println("You current balance is $" + Double.toString(balance) + "0");
 				}
 
-				if (currSelection == 3)
+				if (currSelection == 3) {
 					System.out.println("Unforunately, CDs are not available yet");
+				}
 
 				if (currSelection == 4) {
 					System.out.println("Your current Balance is $" + Double.toString(balance) + "0");
